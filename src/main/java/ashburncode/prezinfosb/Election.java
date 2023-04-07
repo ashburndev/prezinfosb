8222293aa6bf532e1184a5837c777b06d6f521c8
ashburndev <ashburndev@gmail.com>
2023-04-07 07:54:48 -0400

    used eclipse to generate getters setters hashCode equals and toString

--- src/main/java/ashburncode/prezinfosb/Election.java
+++ src/main/java/ashburncode/prezinfosb/Election.java
@@ -3,6 +3,7 @@ package ashburncode.prezinfosb;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Objects;
 
 import jakarta.persistence.*;
 import jakarta.persistence.Id;
@@ -18,7 +19,39 @@ import ashburncode.prezinfosb.President;
 @Entity
 public class Election {
 
-  @Id
+  @Override
+	public String toString() {
+		return "Election [id=" + id + ", winnerPopularVote=" + winnerPopularVote + ", totalPopularVote="
+				+ totalPopularVote + ", winnerElectoralVote=" + winnerElectoralVote + ", totalElectoralVote="
+				+ totalElectoralVote + ", electionDate=" + electionDate + ", incumbWonElection=" + incumbWonElection
+				+ ", president=" + president + "]";
+	}
+
+@Override
+	public int hashCode() {
+		return Objects.hash(electionDate, id, incumbWonElection, president, totalElectoralVote, totalPopularVote,
+				winnerElectoralVote, winnerPopularVote);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Election other = (Election) obj;
+		return Objects.equals(electionDate, other.electionDate) && Objects.equals(id, other.id)
+				&& Objects.equals(incumbWonElection, other.incumbWonElection)
+				&& Objects.equals(president, other.president)
+				&& Objects.equals(totalElectoralVote, other.totalElectoralVote)
+				&& Objects.equals(totalPopularVote, other.totalPopularVote)
+				&& Objects.equals(winnerElectoralVote, other.winnerElectoralVote)
+				&& Objects.equals(winnerPopularVote, other.winnerPopularVote);
+	}
+
+@Id
   @GeneratedValue(strategy = GenerationType.AUTO)
   private Long id;
 
@@ -38,6 +71,70 @@ public class Election {
 
   @ManyToOne
   private President president;
+
+public Long getId() {
+	return id;
+}
+
+public void setId(Long id) {
+	this.id = id;
+}
+
+public Long getWinnerPopularVote() {
+	return winnerPopularVote;
+}
+
+public void setWinnerPopularVote(Long winnerPopularVote) {
+	this.winnerPopularVote = winnerPopularVote;
+}
+
+public Long getTotalPopularVote() {
+	return totalPopularVote;
+}
+
+public void setTotalPopularVote(Long totalPopularVote) {
+	this.totalPopularVote = totalPopularVote;
+}
+
+public Integer getWinnerElectoralVote() {
+	return winnerElectoralVote;
+}
+
+public void setWinnerElectoralVote(Integer winnerElectoralVote) {
+	this.winnerElectoralVote = winnerElectoralVote;
+}
+
+public Integer getTotalElectoralVote() {
+	return totalElectoralVote;
+}
+
+public void setTotalElectoralVote(Integer totalElectoralVote) {
+	this.totalElectoralVote = totalElectoralVote;
+}
+
+public Date getElectionDate() {
+	return electionDate;
+}
+
+public void setElectionDate(Date electionDate) {
+	this.electionDate = electionDate;
+}
+
+public Boolean getIncumbWonElection() {
+	return incumbWonElection;
+}
+
+public void setIncumbWonElection(Boolean incumbWonElection) {
+	this.incumbWonElection = incumbWonElection;
+}
+
+public President getPresident() {
+	return president;
+}
+
+public void setPresident(President president) {
+	this.president = president;
+}
 
 }
 
--- src/main/java/ashburncode/prezinfosb/Term.java
+++ src/main/java/ashburncode/prezinfosb/Term.java
@@ -3,6 +3,7 @@ package ashburncode.prezinfosb;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Objects;
 
 import jakarta.persistence.*;
 import jakarta.persistence.Id;
@@ -22,7 +23,62 @@ public class Term {
   @GeneratedValue(strategy = GenerationType.AUTO)
   private Long id;
 
-  private Date    begDate;
+  @Override
+public String toString() {
+	return "Term [id=" + id + ", begDate=" + begDate + ", endDate=" + endDate + ", president=" + president + "]";
+}
+
+@Override
+public int hashCode() {
+	return Objects.hash(begDate, endDate, id, president);
+}
+
+@Override
+public boolean equals(Object obj) {
+	if (this == obj)
+		return true;
+	if (obj == null)
+		return false;
+	if (getClass() != obj.getClass())
+		return false;
+	Term other = (Term) obj;
+	return Objects.equals(begDate, other.begDate) && Objects.equals(endDate, other.endDate)
+			&& Objects.equals(id, other.id) && Objects.equals(president, other.president);
+}
+
+public Long getId() {
+	return id;
+}
+
+public void setId(Long id) {
+	this.id = id;
+}
+
+public Date getBegDate() {
+	return begDate;
+}
+
+public void setBegDate(Date begDate) {
+	this.begDate = begDate;
+}
+
+public Date getEndDate() {
+	return endDate;
+}
+
+public void setEndDate(Date endDate) {
+	this.endDate = endDate;
+}
+
+public President getPresident() {
+	return president;
+}
+
+public void setPresident(President president) {
+	this.president = president;
+}
+
+private Date    begDate;
   private Date    endDate;
 
   @ManyToOne
